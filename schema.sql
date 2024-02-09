981f2b3186c7353d66d44b841aa038488166a91c
Kaylynn Morgan <mkaylynn7@gmail.com>
2024-02-10 10:47:55 +1100

    Add basic SQL schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS inquisitor;
+
+CREATE TABLE IF NOT EXISTS inquisitor.tracked_games (
+    game_name TEXT PRIMARY KEY,
+    guild_id BIGINT NOT NULL,
+    announcement_channel BIGINT NOT NULL,
+    announcement_role BIGINT
+);
