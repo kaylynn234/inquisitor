2836f55eb738e71ab6bfb49eb1d488e9ca2cc45c
Kaylynn Morgan <mkaylynn7@gmail.com>
2024-02-10 15:52:00 +1100

    Add some basic error handling

--- inquisitor/bot.py
+++ inquisitor/bot.py
@@ -2,9 +2,9 @@ from typing import Any
 
 from aiohttp import ClientSession
 from asyncpg import Pool
-from discord import Intents
+from discord import Forbidden, Intents
 from discord.ext import commands
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, Context, CommandError, CommandInvokeError, CommandNotFound
 
 from .config import Config
 
@@ -27,3 +27,11 @@ class Inquisitor(Bot):
             intents=kwargs.pop("intents", DEFAULT_INTENTS),
             **kwargs,
         )
+
+    async def on_command_error(self, ctx: Context, exception: Exception | CommandError) -> None:
+        exception = exception.original if isinstance(exception, CommandInvokeError) else exception
+
+        if isinstance(exception, (Forbidden, CommandNotFound)):
+            return
+
+        await ctx.send(f"{type(exception).__name__}: {exception}")
